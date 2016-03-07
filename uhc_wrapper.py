860fa90c9d5d67ffe348a17860dc44e68b64fd92
Brian Ronald <brian@ppcis.org>
2016-03-07 00:32:00 +0000

    Simple greeter

--- uhc_wrapper.py
+++ uhc_wrapper.py
@@ -15,12 +15,35 @@ minecraft = pexpect.spawn(commandline,timeout=None,encoding=None)
 running = minecraft.isalive()
 
 while(running):
-    result = minecraft.expect([pexpect.EOF,'You need to agree to the EULA in order to run the server. Go to eula.txt for more info.\r\n'])
+    result = minecraft.expect([
+        #0
+        pexpect.EOF,
+        #1
+        'You need to agree to the EULA in order to run the server. Go to eula.txt for more info.\r\n',
+        #2
+        '\[[0-9]+:[0-9]+:[0-9]+.*INFO\]: ',
+        #3
+        '\[[0-9]+:[0-9]+:[0-9]+.*WARN\]: ',
+        #4
+        '^\w+ joined the game\r\n',
+        #5
+        '\r\n'
+        ])
+    
+    # Show all unmatched output
     print(minecraft.before.replace(b'\r',b'').decode(),end='')
 
     if result == 0:
         print('Server has stopped')
         running = False
     elif result == 1:
-        print(minecraft.after.replace(b'\r',b'').decode(),end='')
         print('\033[31m\033[1meula.txt needs to be completed\033[m')
+    elif result == 2:
+        print('\033[32m'+minecraft.after.replace(b'\r',b'').decode()+'\033[m',end='')
+    elif result == 3:
+        print('\033[33m'+minecraft.after.replace(b'\r',b'').decode()+'\033[m',end='')
+    elif result == 4:
+        print(minecraft.after.replace(b'\r',b'').decode(),end='')
+        minecraft.sendline('say Welcome, ' + minecraft.after.replace(b' joined the game\r\n',b'').decode())
+    elif result == 5:
+        print(minecraft.after.replace(b'\r',b'').decode(),end='')
